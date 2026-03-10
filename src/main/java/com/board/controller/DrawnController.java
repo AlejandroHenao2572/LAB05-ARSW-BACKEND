4e94fa12e046653ed3bb9c23a33aa24d845f531e
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2026-03-09 23:06:20 -0500

    documentacion backend

--- src/main/java/com/board/controller/DrawnController.java
+++ src/main/java/com/board/controller/DrawnController.java
@@ -9,6 +9,12 @@ import com.board.model.DrawMessage;
 @Controller
 public class DrawnController {
 
+    /* 
+     * This method handles draw messages sent via WebSocket.
+     * It is mapped to the /app/draw destination and sends the processed message to all subscribers of the /topic/draw topic.
+     * "DRAW" canvas draws a point at the specified coordinates with the specified color
+     * "ClEAR" canvas clears all drawings
+     */
     @MessageMapping("/draw") // This method will handle messages sent to the /app/draw destination
     @SendTo("/topic/draw") // Specify the destination to which the return value of the method will be sent. In this case, it sends the message to all subscribers of the /topic/draw topic.
     public DrawMessage handleDraw(DrawMessage message) {
