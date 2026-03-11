7db5fd41d957ba83e1b52a3a645e5f25be2fd771
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2026-03-10 20:29:30 -0500

    fix CORS config

--- src/main/java/com/board/config/WebSocketConfig.java
+++ src/main/java/com/board/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.board.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,9 +9,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
-
-    @Value("${allowed.origins}")
-    private String[] allowedOrigins;
     
     /* 
      * This method configures the message broker for handling WebSocket messages.
@@ -35,7 +31,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.
         addEndpoint("/ws-board"). // Conection URL for WebSocket connections
-        setAllowedOrigins(allowedOrigins). // Restrict origins to the configured list
+        setAllowedOriginPatterns("*"). // Allow all origins for CORS (Cross-Origin Resource Sharing)
         withSockJS(); // Register the /ws endpoint for WebSocket connections with SockJS fallback
     }
 }
